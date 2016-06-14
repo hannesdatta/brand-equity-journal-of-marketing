71985997aa7b6185b80ead5a4a1656be6b2f262e
Hannes Datta <hannes@datta-online.de>
2016-06-14 22:52:23 +0200

    add elasticities of non-copula model to output; sink output directly to files

--- report/code/meta_analysis.R
+++ report/code/meta_analysis.R
@@ -1,14 +1,20 @@
-# Set path for focal model
-	path = '../../analysis/output/MNL_copula_5mmix_nomc'
-	require(data.table)
-
+# Load data.table
+require(data.table)
+options(width=1000)
+	
 # Load data 
 	load('..//..//derived//output//datasets.RData')
+
+# Set path for focal models
+	path = '../../analysis/output/MNL_copula_5mmix_nomc'
+	path2 = '../../analysis/output/MNL_non-copula_5mmix_nomc'
 
 # Load results
 	equity <- fread(paste0(path, '//equity.csv'))
 	elast <- fread(paste0(path, '//elasticities.csv'))
 
+	equity_nocop <- fread(paste0(path2, '//equity.csv'))
+	elast_nocop <- fread(paste0(path2, '//elasticities.csv'))
 	
 ###############################
 # Table 3: Sample description #
@@ -37,12 +43,12 @@ nrow(equity[, list(.N), by=c('cat_name', 'brand_name', 'year')])
 write.table(tmp, '../output/sample_description.csv', row.names=F)
 	
 
-# Mean of C4
-
+# Mean of C4 by category
 setkey(elast,cat_name)
 tmp=unique(elast)
+sink('mean_of_c4.txt')
 tmp[, c('cat_name', 'c4'),with=F]
-
+sink()
 	
 ###########################################
 # Table 4: Sales Response Model Estimates #
@@ -98,13 +104,17 @@ summarize_elast <- function(elast) {
 				   negsig = length(unique(id[elast/elast_se<=-sigtest]))/length(unique(id[!is.na(elast)]))
 				   ), by=c('var_name')]
 
-	cat('\n\nSUMMARY OF ELASTICITIES FOR BAV BRANDS\n')
-	print(tmp)
+	#cat('\n\nSUMMARY OF ELASTICITIES FOR BAV BRANDS\n')
+	#print(tmp)
 
 	}
 
-
+sink('elasticities.txt')
+cat('=========================\nModel with copula\n=========================\n')
 summarize_elast(elast)
+cat('\n\n\n=========================\nModel without copulas\n=========================\n')
+summarize_elast(elast_nocop)
+sink()
 
 ##############################
 # CORRELATION TABLE FUNCTION #
@@ -176,7 +186,6 @@ sink('../output/correlations_sbbe.txt')
 print(corstars(as.matrix(tmp[,-c(1:2),with=F], method="pearson", removeTriangle='none', ndec=2)))
 sink()
 
-
 write.table(tmpcor, '../output/correlations_sbbe.csv', row.names=T)
 
 ###########
@@ -212,150 +221,3 @@ sink('../output/correlations_mmix.txt')
 print(corstars(as.matrix(tmpextract[,-c(1:2),with=F]), method="pearson", removeTriangle='none', ndec=2))
 sink()
 
-
-
-
-summarize_equity <- function(equity) {
-	
-	#################################################
-	# Cross-section time-series pooled correlations #
-	#################################################
-	vars=c('sbbe_STD', 'sbbems', 'sbbems_STD', 'annual_avgms_STD', 'unitsales_STD','revenue_STD','price_STD',grep('bav[_].*[_]STD', colnames(equity),value=T))
-	tmp = equity[, c('cat_name', 'brand_name', vars),with=F]
-	tmp=tmp[complete.cases(tmp)]
-	
-	tmpcor=cor(as.matrix(tmp[,-c(1:2),with=F]))
-	
-	
-	cat('\n\nPooled Cross Section - Time Series Correlations Across Brands and Time\n\n')
-	print(tmpcor)
-	cat(paste0('\nNote: Measured for ', length(unique(paste0(tmp$brand_name,tmp$cat_name))), ' unique BAV brands\n'))
-	
-	}
-	
-	
-	
-	
-summ <- function(tmp_results)	{
-	
-	cat('\n\nSELECTED DECAY PARAMETERS FOR ADVERTISING\n')
-	decay=rbindlist(lapply(tmp_results, function(x) data.frame(cat_name=x$cat_name, adv_decay=.01*as.numeric(x$adv_decay))))
-	print(summary(decay$adv_decay))
-	cat('\nDecay parameters for all categories:\n')
-	print(decay)
-	
-	}	
-	
-
-for (r in models) {
-	
-	source('proc_report.R')
-	options(width=1000)
-	sink(paste0('..//output//', r, '//summary_', r, '.txt'))
-	summarize_elast(elast)
-	summarize_equity(equity)
-	
-	#####################
-	# Regressions: SBBE #
-	#####################
-	
-	cat('\n\n===========================================================\nMode 1: SBBE (intercepts) regressed on BAV Factors\n===========================================================\n')
-	m<-lm(sbbe_STD~1+
-			   F_RelEstKnow_STD+F_EnergDiff_STD, data = equity, subset = !is.na(F_RelEstKnow), weights=1/sbbe_se)
-
-	print(summary(m))
-	
-	equity[, ':=' (seccat_mc = seccat - mean(ifelse(is.na(F_RelEstKnow_STD),NA, seccat),na.rm=T),
-				   newbrnd_mc = newbrnd - mean(ifelse(is.na(F_RelEstKnow_STD),NA, newbrnd),na.rm=T))]
-	
-	
-	cat('\n\n===========================================================\nModel 2: SBBE (intercepts) regressed on BAV Factors and brand characteristics\n===========================================================\n')
-	m<-lm(sbbe_STD~1+
-			   F_RelEstKnow_STD+F_EnergDiff_STD + seccat + newbrnd, data = equity, subset = !is.na(F_RelEstKnow), weights=1/sbbe_se)
-
-	print(summary(m))
-
-	cat('\n\n===========================================================\nModel 3: SBBE (intercepts) regressed on BAV Factors and brand characteristics with interactions\n===========================================================\n')
-	m<-lm(sbbe_STD~1+
-			   F_RelEstKnow_STD+F_EnergDiff_STD + seccat_mc + newbrnd_mc +
-			   F_RelEstKnow_STD * seccat_mc + F_EnergDiff_STD * seccat_mc +
-			   F_RelEstKnow_STD * newbrnd_mc + F_EnergDiff_STD * newbrnd_mc, data = equity, subset = !is.na(F_RelEstKnow), weights=1/sbbe_se)
- 
-	print(summary(m))
-
-	cat('\n\n===========================================================\nModel 4: Averaged SBBE intercepts, regressed on average BAV Factors and brand characteristics with interactions\n===========================================================\n')
-	
-	equity_avg = equity[!is.na(F_RelEstKnow_STD), lapply(.SD, mean), by=c('cat_name', 'brand_name'), .SDcols=c('sbbe_STD', 'F_RelEstKnow_STD', 'F_EnergDiff_STD', 'sbbe_se', 'seccat', 'newbrnd')]
-	
-	equity_avg[, ':=' (seccat_mc = seccat - mean(seccat),
-				   newbrnd_mc = newbrnd - mean(F_RelEstKnow_STD))]
-	
-	m<-lm(sbbe_STD~1+
-			   F_RelEstKnow_STD+F_EnergDiff_STD + seccat_mc + newbrnd_mc +
-			   F_RelEstKnow_STD * seccat_mc + F_EnergDiff_STD * seccat_mc +
-			   F_RelEstKnow_STD * newbrnd_mc + F_EnergDiff_STD * newbrnd_mc, data = equity_avg)#, weights=1/sbbe_se)
- 
-	print(summary(m))
-
-	#############################
-	# Regressions: Elasticities #
-	#############################
-	
-	# select BAV brands only
-	# compute mean-centered interaction effects
-	
-	signific = c(0.001, 0.01, .050, .1)
-	names(signific) <- c('****', '***', '**', '*')
-	options(signif.symbols=signific)
-
-	elastreg<-function(form, weights, standardize=FALSE, dt, msg=NULL) {
-		# do by variable
-		if (!is.null(weights)) eval(parse(text=paste0('dt[, w:=', weights,']'))) else dt[, w:=1]
-		vars = unique(dt$var_name)
-		meta<-NULL
-		for (j in seq(along=vars)) { #as.factor(cat_name) +
-			meta[[j]] <- lm(as.formula(form), data= dt, subset= !is.na(dt)&var_name==vars[j], weights=w)#(1/elast_se))
-			}
-		#!elast_outlier & 
-		require(car)
-		require(memisc)
-		
-		m_all = paste0(paste0('\"',vars,'\"'),'=meta[[',seq(along=vars),']]')
-		
-		cat('\n\n\nMeta analysis')
-		#print(eval(parse(text=paste0('mtable(', paste0(m_all, collapse=','), ',coef.style=\"all\")'))))
-		print(eval(parse(text=paste0('mtable(', paste0(m_all, collapse=','), ')'))))
-		
-		#,signif.symbols=signific)
-		# 
-		cat(paste0('\n', msg, '\n'))
-	
-		cat('\nVIF values\n')
-		for (j in seq(along=meta)) {
-			cat('\n', vars[j], '\n')
-			print(vif(meta[[j]]))
-			}
-	}
-
-	cat('\n\n======================================\n')
-	cat('======================================\nElasticities regressed on BAV Factors\n======================================\n')
-	cat('======================================\n\n\n')
-	
-	cat('\n\nElasticities regressed on BAV Factors for all BAV brands\n=========================================================\n')
-	uniq_br=elast[!is.na(bav_asset)][, list(.N),by=c('cat_name', 'brand_name')]
-	elastreg('elast_STD ~ 1 + F_RelEstKnow_STD + F_EnergDiff_STD', weights='1/elast_se', dt=elast[!is.na(bav_asset)], msg=paste0('Models estimated with all ', nrow(uniq_br), ' BAV brands.'))
-	
-	#cat('\n\nElasticities regressed on BAV Factors for main-category BAV brands only\n=========================================================\n')
-	#uniq_br=elast[seccat==0 & !is.na(bav_asset)][, list(.N),by=c('cat_name', 'brand_name')]
-	#elastreg('elast_STD ~ 1 + F_RelEstKnow_STD + F_EnergDiff_STD', weights='1/elast_se', dt=elast[seccat==0& !is.na(bav_asset)], msg=paste0('Models estimated with ', nrow(uniq_br), ' brands in their main categories #(seccat==0).'))
-	
-	sink()
-	
-	############
-	# PLOTTING #
-	############
-	
-	source('summary_plots.R')
-
-	
-	}
